a207b2f2439159918c6e206a8c1416261def2bd7
Avinash Chandra Srivastava <er.srivastava@gmail.com>
2022-09-14 16:37:47 +0100

    added endpoints

--- aws-vpc/vpc.tf
+++ aws-vpc/vpc.tf
@@ -79,43 +79,37 @@ resource "aws_network_acl_rule" "private_nacl_rules_out" {
 
 # DynamoDB & s3 endpoint and it's route
 data "aws_vpc_endpoint_service" "s3" {
-  count        = var.enable_s3_vpc_endpoint ? 1 : 0
   service      = "s3"
   service_type = "Gateway"
 }
 
 resource "aws_vpc_endpoint" "s3" {
-  count        = var.enable_s3_vpc_endpoint ? 1 : 0
   vpc_id       = aws_vpc.demo.id
   service_name = data.aws_vpc_endpoint_service.s3[0].service_name
   tags = {
-    Name = "${var.project}-s3-endpoint"
+    Name = "${var.project}-s3-endpoint-${var.environment}"
   }
 }
 
 resource "aws_vpc_endpoint_route_table_association" "private_s3" {
-  count           = var.enable_s3_vpc_endpoint ? 1 : 0
   vpc_endpoint_id = aws_vpc_endpoint.s3[0].id
   route_table_id  = aws_route_table.private_route_table.id
 }
 
 data "aws_vpc_endpoint_service" "dynamodb" {
-  count        = var.enable_dynamodb_vpc_endpoint ? 1 : 0
   service      = "dynamodb"
   service_type = "Gateway"
 }
 
 resource "aws_vpc_endpoint" "dynamodb" {
-  count        = var.enable_dynamodb_vpc_endpoint ? 1 : 0
   vpc_id       = aws_vpc.demo.id
   service_name = data.aws_vpc_endpoint_service.dynamodb[0].service_name
   tags = {
-    Name = "${var.project}-dynamodb-endpoint"
+    Name = "${var.project}-dynamodb-endpoint-${var.environment}"
   }
 }
 
 resource "aws_vpc_endpoint_route_table_association" "private_dynamodb" {
-  count           = var.enable_dynamodb_vpc_endpoint ? 1 : 0
   vpc_endpoint_id = aws_vpc_endpoint.dynamodb[0].id
   route_table_id  = aws_route_table.private_route_table.id
 }
