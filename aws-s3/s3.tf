f2f2c29e7511d8c62543ac4ea1b21d715af6e44d
Avinash Chandra Srivastava <er.srivastava@gmail.com>
2022-09-14 10:10:39 +0100

    added s3 tf module

--- aws-s3/s3.tf
+++ aws-s3/s3.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "repo" {
-  bucket = var.bucket_name
+  bucket        = var.bucket_name
+  force_destroy = var.force_destroy
 
   tags = {
     Name        = var.bucket_name
